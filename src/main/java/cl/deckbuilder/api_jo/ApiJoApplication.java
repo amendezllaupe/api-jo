fee560066f4de74435c0d8afb34e57e08ed8c9ca
amendezllaupe <amendezllaupe@gmail.com>
2025-08-24 19:54:48 -0400

    Init Springboot

---
+++ src/main/java/cl/deckbuilder/api_jo/ApiJoApplication.java
@@ -0,0 +1,13 @@
+package cl.deckbuilder.api_jo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiJoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiJoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/cl/deckbuilder/api_jo/ApiJoApplicationTests.java
@@ -0,0 +1,13 @@
+package cl.deckbuilder.api_jo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiJoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
